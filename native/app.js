4ba630c4640105921b418934a4340abb4f9215d5
spring <spring.wind2006@163.com>
2021-11-11 18:34:25 +0800

    update

--- native/app.js
+++ native/app.js
@@ -1,4 +1,10 @@
 const util=require("./lib/util");
-const stats=require("../data/wordStats.json");
-const datas=require("../data/export/basic.json");
-const unkowns=require("../data/unkown.json");
+let hexagrams=util.readJson("./data/export/hexagram.json");
+
+console.log(hexagrams);
+hexagrams.forEach(e=>{
+    if(typeof(e.analysis)=='string'){
+        e.analysis=[e.analysis];
+    }
+})
+
